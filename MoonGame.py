a58e7f2e904e56706c037b8f649ec4133846af10
Stefano Dutto <117385811+stefano0309@users.noreply.github.com>
2025-10-21 16:51:35 +0200

    Aggiunta win condition aggiunti livelli difficolta e best score modificato il README.md e unito alcune funzioni in una sola

--- MoonGame.py
+++ MoonGame.py
@@ -1,5 +1,7 @@
 import pygame
 import time
+import math
+import os
 import random
 
 pygame.init()
@@ -27,7 +29,10 @@ background = pygame.image.load("images/background.jpg").convert_alpha()
 title = pygame.image.load("images/title.png").convert_alpha()
 title_rect = title.get_rect(center=(350, 100))
 
-#stage 1
+
+#stage 1 gameplay
+
+#funzioni punteggio basato sul timer ovvero sul numero di meteoriti evitati
 def point(timer):
     if timer//10 >= 1:
         point = timer * (timer/10)
@@ -35,21 +40,32 @@ def point(timer):
         point = 0
     return point
 
-def speed(timer):
-    level = timer // 10 +1
+
+#funzioni livelli basato sul timer ovvero sul numero di meteoriti evitati
+def level (timer):
+    level = timer // 10 + 1
     speed = 1 + level * 0.5
-    if speed >= 3:
-        speed = 3
-    return speed
-
-timer = 0
-
-#stage 2
-go = pygame.image.load("images/gameOver.png").convert_alpha()
-go_rect = go.get_rect(center=(350, 100))
-
-#point text
-font_var = pygame.font.Font("font/Pixels.ttf", 50)
+    if level <= 5:
+        dif = "very easy"
+        if speed >= 2:
+            speed = 2
+    elif level <= 10:
+        dif = "easy"
+        if speed >= 3:
+            speed = 3
+    elif level <= 15:
+        dif = "medium"
+        if speed >= 3.5:
+            speed = 3.5
+    elif level <= 20:
+        dif = "hard"
+        if speed >= 4:
+            speed = 4
+    elif level <= 25:
+        dif = "very hard"
+        if speed >= 4.5:
+            speed = 4.5
+    return level, dif, speed
 
 
 #Play button
@@ -72,14 +88,28 @@ for i in range(5):
     y = random.randint(-200, -100)
     meteor_pos.append(pygame.FRect(x, y, 50, 50))
 
-
-
 #player
 player = pygame.image.load("images/player.png").convert_alpha()
 player_red = pygame.transform.scale(player, (90,100))
 player_rect = player_red.get_rect(center=(350, 200))
 player_hitbox = player_rect.inflate(-55, -50)
 
+timer = 0
+
+#Miglior punteggio
+if os.path.exists("best_score.txt"):
+    with open("best_score.txt", "r") as f:
+        if os.stat("best_score.txt").st_size == 0:
+            best_score = 0
+        else:
+            best_score = float(f.read())
+            f.close()
+#stage 2 game over
+go = pygame.image.load("images/gameOver.png").convert_alpha()
+go_rect = go.get_rect(center=(350, 100))
+
+#point text
+font_var = pygame.font.Font("font/Pixels.ttf", 50)
 
 #Stage cond
 start = False
@@ -101,7 +131,7 @@ while running:
             if play_rect.collidepoint(event.pos):
                 if stage == 0:
                     start = True
-                if stage == 2:
+                else:
                     # reset posizioni del gioco
                     stage = 1
                     player_rect.center = (300, 200)
@@ -116,47 +146,54 @@ while running:
                 running = False
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 kd = True                 
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                 ka = True
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
                 kw = True
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 ks = True
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_d:
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 kd = False
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                 ka = False
-            if event.key == pygame.K_w:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
                 kw = False
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 ks = False
     screen.blit(background, (0,0))
 
-    #start
+    #stage 0
     if stage == 0:
         screen.blit(title, title_rect)
         screen.blit(play_red, play_rect)
         screen.blit(quit_red, quit_rect)
+        best_score_text = font_var.render("best score: "+ str(best_score), False, (255, 255, 255))
+        best_score_rect = best_score_text.get_rect(center=(350, 250))
+        screen.blit(best_score_text, best_score_rect)
 
-    #game
+    #stage 1
     if stage == 1:
         pygame.mixer.music.pause() 
+        level_text = font_var.render("Level: "+ str(level(timer)[0])+ " Dif: "+str(level(timer)[1]), False, (255, 255, 255))
+        level_rect = level_text.get_rect(center=(350, 25))
+        screen.blit(level_text, level_rect)
         screen.blit(player_red, player_rect)
         #debug player
         #pygame.draw.rect(screen, "red", player_hitbox, 2)
         for meteor_rect in meteor_pos:
-            meteor_rect.y += 2 * speed(timer)
-            screen.blit(meteor_red, meteor_rect)
+            meteor_rect.y += 2 * level(timer)[2]
+            if level(timer)[0] < 26:
+                screen.blit(meteor_red, meteor_rect)
             if meteor_rect.top > h:
                 meteor_rect.x = random.randint(0, w-50)
                 meteor_rect.y = random.randint(-200, -100)
                 timer += 1
             #debug meteor
             #pygame.draw.rect(screen, "red", meteor_rect, 2)
-            if player_hitbox.colliderect(meteor_rect):
+            if player_hitbox.colliderect(meteor_rect) and level(timer)[0] < 26:
                 pygame.mixer.Sound("effect/crash.mp3").play()
                 time.sleep(2)
                 volume = 0
@@ -165,10 +202,21 @@ while running:
                 #print(timer)
                 #print(point(timer))
                 #print("Game Over")
+        if level(timer)[0] > 25:
+            ka , kd , kw , ks = False, False, False, False
+            player_rect.y -= 2 * level(timer)[2]
+            player_hitbox.y -= 2* level(timer)[2]
+            if player_rect.top <= 0:
+                stage = 3 #win condition
 
-    #game over
+    #stage 2
     if stage == 2:
         volume += 0.003
+        if point(timer) > best_score:
+            best_score = point(timer)
+            with open("best_score.txt", "w") as f:
+                f.write(str(best_score))
+            f.close()
         if volume >= 0.25:
             volume = 0.25
         pygame.mixer.music.set_volume(volume)
@@ -178,10 +226,27 @@ while running:
         screen.blit(play_red, play_rect)
         quit_rect.center = (450, 200)
         screen.blit(quit_red, quit_rect)
-        point_text = font_var.render("Points: "+ str(point(timer)), False, (255, 255, 255))
+        point_text = font_var.render("Points: "+ str(math.floor(point(timer))), False, (255, 255, 255))
         point_rect = point_text.get_rect(center=(350, 250))
         screen.blit(point_text, point_rect)
-        
+    
+    #stage 3
+    if stage == 3:
+        volume += 0.003
+        if volume >= 0.25:
+            volume = 0.25
+        pygame.mixer.music.set_volume(volume)
+        pygame.mixer.music.unpause() 
+        win_text = font_var.render("You Win!", False, (255, 255, 255))
+        win_rect = win_text.get_rect(center=(350, 150))
+        screen.blit(win_text, win_rect)
+        play_rect.center = (250, 250)
+        screen.blit(play_red, play_rect)
+        quit_rect.center = (450, 250)
+        screen.blit(quit_red, quit_rect)
+        point_text = font_var.render("Points: "+ str(point(timer)), False, (255, 255, 255))
+        point_rect = point_text.get_rect(center=(350, 200))
+        screen.blit(point_text, point_rect)
         
     
     #start animation
@@ -191,6 +256,7 @@ while running:
             volume = 0
         pygame.mixer.music.set_volume(volume)
         title_rect.y -= 2
+        best_score_rect.y += 2
         play_rect.x -= 4
         quit_rect.x += 4
         if title_rect.bottom <= 0 and play_rect.right <= 0 and quit_rect.left >= w and volume == 0:
@@ -212,18 +278,18 @@ while running:
 
     #Movimenti su x
     if kd:
-        player_rect.x += 2 * speed(timer)
-        player_hitbox.x += 2* speed(timer)
+        player_rect.x += 2 * level(timer)[2]
+        player_hitbox.x += 2* level(timer)[2]
     if ka:
-        player_rect.x -= 2 * speed(timer)
-        player_hitbox.x -= 2* speed(timer)
+        player_rect.x -= 2 * level(timer)[2]
+        player_hitbox.x -= 2* level(timer)[2]
     #Movimenti su y
     if kw:
-        player_rect.y -= 2 * speed(timer)
-        player_hitbox.y -= 2* speed(timer)
+        player_rect.y -= 2 * level(timer)[2]
+        player_hitbox.y -= 2* level(timer)[2]
     if ks:
-        player_rect.y += 2 * speed(timer)
-        player_hitbox.y += 2* speed(timer)
+        player_rect.y += 2 * level(timer)[2]
+        player_hitbox.y += 2* level(timer)[2]
     
 
     pygame.display.update()
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # MoonGame
 
 Gioco in sitle arcade di una navicella spaziale che deve schivare degli asteroidi con il passare del tempo la velocità aumenta e di conseguenza la difficolta.
+
+L'eseguibile è creato con:
+    pyinstaller --onefile --windowed --icon=icon.ico moon_game.py
